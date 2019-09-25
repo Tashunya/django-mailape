8a0fd00efab912f1b7aab7f27a538d116c97e192
Tashunya <tashunya_17@mail.ru>
2019-09-25 15:06:07 +0300

    Add Mailinglist view

--- mailape/settings.py
+++ mailape/settings.py
@@ -59,8 +59,9 @@ ROOT_URLCONF = 'mailape.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
--- mailinglist/urls.py
+++ mailinglist/urls.py
@@ -4,5 +4,5 @@ from mailinglist import views
 app_name = 'mailinglist'
 
 urlpatterns = [
-
+    path('', views.MailingListListView.as_view(), name='mailinglist_list'),
 ]
