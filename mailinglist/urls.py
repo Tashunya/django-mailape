3a6297519eee18c027704b45b276dfd5c26b63b6
Tashunya <tashunya_17@mail.ru>
2019-09-25 15:18:38 +0300

    Add create mailing list view

--- mailinglist/urls.py
+++ mailinglist/urls.py
@@ -5,4 +5,5 @@ app_name = 'mailinglist'
 
 urlpatterns = [
     path('', views.MailingListListView.as_view(), name='mailinglist_list'),
+    path('new', views.CreateMailingListView.as_view(), name='create_mailinglist'),
 ]
