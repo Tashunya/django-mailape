b93d2c75d2eb10bfd12030f7b8453988d32bce36
Tashunya <tashunya_17@mail.ru>
2019-09-27 17:26:17 +0300

    Add sending message to subscribers

--- mailape/mailinglist/tasks.py
+++ mailape/mailinglist/tasks.py
@@ -4,7 +4,21 @@ from . import emails
 
 @shared_task # turns a function into a Task
 def send_confirmation_email_to_subscriber(subscriber_id):
+    # import model inside to prevent cyclical import error
     from .models import Subscriber
     subscriber = Subscriber.objects.get(id=subscriber_id)
     emails.send_confirmation_email(subscriber)
 
+
+@shared_task
+def build_subscriber_messages_for_message(message_id):
+    from .models import Message, SubscriberMessage
+    message = Message.objects.get(id=message_id)
+    SubscriberMessage.objects.create_from_message(message)
+
+
+@shared_task
+def send_subscriber_message(subscriber_message_id):
+    from .models import SubscriberMessage
+    subscriber_message = SubscriberMessage.objects.get(id=subscriber_message_id)
+    emails.send_subscriber_message(subscriber_message)
